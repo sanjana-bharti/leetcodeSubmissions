374b1d2b78f2934aa482f9b438097cdac3badb55
sanjana-bharti <114351569+sanjana-bharti@users.noreply.github.com>
2022-11-17 17:49:29 +0530

    Time: 447 ms (39.33%), Space: 14.4 MB (53.29%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -6,44 +6,51 @@ public:
             return true;
         return false;
     }
-    void dfs(map<int,vector<int>>& mp,vector<int>& visited,int s)
+    int find(vector<int>& parent,int x)
     {
-        visited[s]=1;
-        vector<int> adj=mp[s];
-        int i;
-        for(i=0;i<adj.size();i++)
-        {
-            if(!visited[adj[i]])
-                dfs(mp,visited,adj[i]);
-        }
+        if(x==parent[x])
+            return x;
+        return parent[x]=find(parent,parent[x]);
     }
-    int removeStones(vector<vector<int>>& stones) 
+    int performUnion(vector<int>& parent, vector<int>& rank,int x,int y)
     {
-        int n=stones.size();
-        int i,j;
-        map<int,vector<int>> adj;
-        for(i=0;i<n-1;i++)
+        x=find(parent,x);
+        y=find(parent,y);
+        if(x==y)
+            return 0;
+        if(rank[x]>=rank[y])
+        {
+            parent[y]=x;
+            rank[x]+=rank[y];
+        }
+        else
+        {
+            parent[x]=y;
+            rank[y]+=rank[x];
+        }
+        return 1;
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        int n,i,j;
+        n=stones.size();
+        vector<int> parent(n);
+        vector<int> rank(n);
+        for(i=0;i<n;i++)
+        {
+            parent[i]=i;
+            rank[i]=1;
+        }
+        int components=n;
+        for(i=0;i<n;i++)
         {
             for(j=i+1;j<n;j++)
             {
                 if(shareEdge(stones[i],stones[j]))
                 {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                 components-=performUnion(parent,rank,i,j);   
                 }
             }
         }
-        vector<int> visited(n,0);
-        int components=0;
-        for(i=0;i<n;i++)
-        {
-            if(!visited[i])
-            {
-                components++;
-                dfs(adj,visited,i);
-            }
-        }
         return n-components;
-        
     }
 };
